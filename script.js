cd862abe87739b7a1b22fdf4ee12b0b4a8a0f0bd
Nestorillo <Nestorillo.golfo@gmail.com>
2025-06-06 16:50:28 +0200

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// Scroll 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+const hiddenElements = document.querySelectorAll('.project');
+hiddenElements.forEach(el => observer.observe(el));
+
+// Button Up
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+// Scroll Button
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
